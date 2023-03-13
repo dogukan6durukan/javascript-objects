e01846c43128cd3eef7c6a37dfd4dc48b07ccf48
dokuzor <dokuzorofficial@gmail.com>
2023-03-13 14:12:45 +0300

    first commit

---
+++ objects/index.js
@@ -0,0 +1,185 @@
+// * CREATE A METHOD TO READING GIVEN OBJECT PROPERTIES
+
+const userForReadingProperties = {
+    name: "John",
+    age: 32,
+    greeting: ["Hello!", "Hola!", "Hallo!"],
+}
+  
+function readProperties(obj) {
+  const properties = Object.keys(obj);
+
+  if (properties.length < 1) return;
+
+  for (let key of properties) console.log(obj[key]);
+}
+
+// readProperties(userForReadingProperties);
+
+// * CREATE A METHOD TO DELETE GIVEN OBJECT PROPERTIES
+
+function deleteProperties(obj, ...deletedPropertyNames) {
+  const properties = Object.keys(obj);
+
+  if (properties.length < 1) return;
+
+  if (deletedPropertyNames.length < 1) return;
+
+  // FIND AND DELETE PROPERTIES
+  return deletedPropertyNames.filter((p) => {
+    for (let key of properties) {
+      if (p === key) {
+        return delete obj[key];
+      }
+    }
+  });
+}
+
+const userForDeletingProperties = {
+  name: "Dogukan",
+  age: 16,
+  skills : ["javascript", "react"]
+};
+
+deleteProperties(userForDeletingProperties, "age", "name");
+
+// console.log(userForDeletingProperties); //skills: (2) ['javascript', 'react']
+
+
+// * CREATE A METHOD FOR MAKING DEEP OBJECT COMPARASION
+
+const user1 = {
+  name: "Dogukan",
+  age: 17,
+  alive: undefined,
+  admin: null,
+  skills: {
+    frontend: {
+      markup: ["html", "css"],
+      interaction: ["javascript"],
+      frameworks: {
+        markup: ["tailwind"],
+        interaction: ["react", "next", "astro"],
+      },
+    },
+
+    backend: {
+      technologies: ["nodejs", "php"],
+      frameworks: {
+        techFrameworks: ["fastify", "express", "laravel"],
+      },
+    },
+  },
+};
+
+const user2 = {
+  name: "Dogukan",
+  age: 17,
+  alive: undefined,
+  admin: null,
+  skills: {
+    frontend: {
+      markup: ["html", "css"],
+      interaction: ["javascript"],
+      frameworks: {
+        markup: ["tailwind"],
+        interaction: ["react", "next", "astro"],
+      },
+    },
+
+    backend: {
+      technologies: ["nodejs", "php"],
+      frameworks: {
+        techFrameworks: ["fastify", "express", "laravel"],
+      },
+    },
+  },
+};
+
+function areEqual(obj1, obj2) {
+  if (typeof obj1 === "object" && typeof obj2 === "object") {
+    const obj1props = Object.keys(obj1);
+    const obj2props = Object.keys(obj2);
+
+    if (obj1props.length !== obj2props.length) return false;
+
+    return obj1props.every((key) => {
+      if (obj2.hasOwnProperty(key)) {
+        if (typeof obj1[key] === "object" && obj1[key] !== null) {
+          return areEqual(obj1[key], obj2[key]);
+        }
+        return obj1[key] === obj2[key];
+      }
+
+      return false;
+    });
+  }
+}
+
+
+// console.log(areEqual(user1, user2)); // true
+
+
+
+// * CREATE A METHOD FOR MAKING DEEP AGE CALCULATOR
+
+const users = {
+  user1: {
+    name: "dogukan",
+    surname: "durukan",
+    age: 17,
+    siblings: {
+      sibling1: {
+        name: "John",
+        surname: "Doe",
+        bestFriends: {
+          age: [16, 18, 24, { age: 39 }],
+        },
+      },
+    },
+  },
+};
+
+const allAges = [];
+
+function findAllAges(obj) {
+  const keys = Object.keys(obj);
+
+  if (keys.length < 1) return;
+
+  for (let key of keys) {
+    if (key === "age") {
+      if (typeof obj[key] === "number") {
+        allAges.push(obj[key]);
+      }
+      if (Array.isArray(obj[key])) {
+        extractArr(obj[key]);
+      }
+    }
+
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      findAllAges(obj[key]);
+    }
+  }
+
+  return calculateAges(allAges);
+}
+
+function calculateAges(arr) {
+  if (arr.length < 1) return;
+  const fullYear = new Date().getFullYear();
+  return arr.map((age) => {
+    return fullYear - age;
+  });
+}
+
+function extractArr(arr) {
+  if (arr.length < 1) return;
+  for (let key of arr) {
+    if (typeof key === "number") {
+      allAges.push(key);
+    }
+  }
+}
+
+// console.log(findAllAges(users)); // [2006, 2007, 2005, 1999]
